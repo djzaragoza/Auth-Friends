84d1c246684b38ae3f6707c8ae5159c7cd717971
DJ Zaragoza <zaragoza0126@gmail.com>
2019-08-02 18:45:26 -0700

    finished index.js file for now

--- friends/src/index.js
+++ friends/src/index.js
@@ -8,9 +8,8 @@ import './index.css';
 import App from './App';
 import rootReducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+
+
