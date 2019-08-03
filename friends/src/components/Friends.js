b2f45923fd9d95cb22fca224dfae032e8e328618
DJ Zaragoza <zaragoza0126@gmail.com>
2019-08-02 21:07:15 -0700

    finished friends.js file with some bugs

---
+++ friends/src/components/Friends.js
@@ -0,0 +1,33 @@
+import React rom 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+
+import Friend from './Friend';
+import {fetchFriends} from '../actions/index';
+
+class Friends extends React.Component {
+    componentDidMount() {
+        this.props.fetchFriends();
+    }
+
+    render() {
+        return (
+            <div>
+                {this.props.friends.map(friend => {
+                    <Friend friend={friend} />
+                })}
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => ({
+    friends: state.friends,
+});
+
+export default withRouter (
+    connect(
+        mapStateToProps,
+        { fetchFriends }
+    )(Friends)
+);
