f7478a1430622a7a86e6efe40384a685919ea84f
DJ Zaragoza <zaragoza0126@gmail.com>
2019-08-02 18:42:29 -0700

    adding all necessary imports inside index.js file

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import rootReducer from './reducers';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
